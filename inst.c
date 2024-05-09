7617338f7e424e5ca373ff37692948f3e4d5d439
Amittai Aviram <afa13@columbia.edu>
2024-05-09 12:11:00 -0400

    Fixed bug: mem expressions as operands to test instruction.

--- inst.c
+++ inst.c
@@ -684,7 +684,7 @@ void I_trans_stm(I_Function func, TR_Stm stm) {
         case TR_IF_STM:
             {
                 I_Operand test_result = I_trans_exp(func, stm->u.if_.test);
-                if (test_result->kind == I_IMM_OPRND) {
+                if (test_result->kind != I_REG_OPRND) {
                     test_result =
                         I_move_to_temp_reg(func, test_result, stm->u.if_.test->size);
                 }
@@ -703,7 +703,7 @@ void I_trans_stm(I_Function func, TR_Stm stm) {
         case TR_IF_ELSE_STM:
             {
                 I_Operand test_result = I_trans_exp(func, stm->u.if_else.test);
-                if (test_result->kind == I_IMM_OPRND) {
+                if (test_result->kind != I_REG_OPRND) {
                     test_result =
                         I_move_to_temp_reg(func, test_result, stm->u.if_else.test->size);
                 }
